efbf0643d1aaab66926c30816fb8c97e8f870d15
Rehan <mohdaftabalamsayyed@gmail.com>
2025-02-10 22:12:02 +0530

    New Features added

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-//351 TV
 const input = document.getElementById("input-box");
 const list = document.querySelector(".todo-list");
 const btn = document.getElementById("add");
-btn.addEventListener('click',()=>{
-    if(input.value===''){
-        alert("You Must write somethings!!!");
-    }
-    else{
-        const li = document.createElement('li');
-        li.className="todo-item";
+
+btn.addEventListener("click", () => {
+    if (input.value === "") {
+        alert("You Must write something!!!");
+    } else {
+        const li = document.createElement("li");
+        li.className = "todo-item";
         li.innerHTML = input.value;
         list.appendChild(li);
-        const btn1 = document.createElement('button');
-        btn1.className="todo-item button";
-        btn1.innerHTML = "Delete"
+
+        const btn1 = document.createElement("button");
+        btn1.className = "todo-item button";
+        btn1.innerHTML = "Delete";
         li.appendChild(btn1);
-        btn1.addEventListener('click',()=>{
+
+        btn1.addEventListener("click", () => {
             li.remove();
-        })
+        });
     }
-    input.value="";
-})
+    input.value = "";
+});
 
-
+// Fix: Use event delegation to apply line-through on clicked <li>
+list.addEventListener("click", (event) => {
+    if (event.target.tagName === "LI") {
+        event.target.style.textDecoration =
+            event.target.style.textDecoration === "line-through"
+                ? "none"
+                : "line-through";
+    }
+});
